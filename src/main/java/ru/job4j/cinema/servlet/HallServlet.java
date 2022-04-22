c598f60ec637c5d19e203d58ae51e73a11157a69
ftptpf <inboxru@yandex.ru>
2022-04-22 09:49:00 +0300

    2. Сервис - Кинотеатр [#3236] - HallServlet

---
+++ src/main/java/ru/job4j/cinema/servlet/HallServlet.java
@@ -0,0 +1,19 @@
+package ru.job4j.cinema.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HallServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
