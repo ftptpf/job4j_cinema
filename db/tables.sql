df47f6d8f6387e8edd31b5767f815f4cd1a2f706
ftptpf <inboxru@yandex.ru>
2022-04-29 16:29:02 +0300

    2. Сервис - Кинотеатр [#3236] - row sell

--- db/tables.sql
+++ db/tables.sql
@@ -10,5 +10,20 @@ CREATE TABLE ticket (
     session_id INT NOT NULL,
     row INT NOT NULL,
     cell INT NOT NULL,
-    account_id INT NOT NULL REFERENCES account(id)
+    account_id INT NOT NULL REFERENCES account(id),
+    CONSTRAINT unique_ticket UNIQUE (session_id, row, cell)
 );
+
+INSERT INTO account(id, username, email, phone) VALUES (0, 'somebody', 'some@email.ru', 0);
+
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 1, 1, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 1, 2, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 1, 3, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 2, 1, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 2, 2, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 2, 3, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 3, 1, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 3, 2, 0);
+INSERT INTO ticket(session_id, row, cell, account_id) VALUES (1, 3, 3, 0);
+
+INSERT INTO account(username, email, phone) VALUES ('Николай', 'mail@mail.ru', '+74951112233');
